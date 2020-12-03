3e7e12ab3084d4cab662c0862f7ce00641384dc2
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-12-03 14:36:02 +0000

    Introduce from_short_depth and to_short_depth.

--- smg/utility/image_util.py
+++ smg/utility/image_util.py
@@ -18,6 +18,17 @@ class ImageUtil:
         return np.ascontiguousarray(img[:, :, [2, 1, 0]])
 
     @staticmethod
+    def from_short_depth(short_depth_image: np.ndarray, *, depth_scale_factor: float = 1000) -> np.ndarray:
+        """
+        Convert an unsigned short depth image to a floating-point one.
+
+        :param short_depth_image:   The unsigned short depth image.
+        :param depth_scale_factor:  The factor by which to divide the depths during the conversion.
+        :return:                    The floating-point depth image.
+        """
+        return short_depth_image / depth_scale_factor
+
+    @staticmethod
     def load_depth_image(filename: str, *, depth_scale_factor: float = 1000.0) -> np.ndarray:
         """
         Load a depth image from disk.
@@ -26,7 +37,9 @@ class ImageUtil:
         :param depth_scale_factor:  The factor by which the depths were scaled when they were saved.
         :return:                    The loaded depth image.
         """
-        return cv2.imread(filename, cv2.IMREAD_UNCHANGED) / depth_scale_factor
+        return ImageUtil.from_short_depth(
+            cv2.imread(filename, cv2.IMREAD_UNCHANGED), depth_scale_factor=depth_scale_factor
+        )
 
     @staticmethod
     def save_depth_image(filename: str, depth_image: np.ndarray, *, depth_scale_factor: float = 1000) -> None:
@@ -37,5 +50,15 @@ class ImageUtil:
         :param depth_image:         The depth image to save.
         :param depth_scale_factor:  The factor by which to scale the depths before saving them.
         """
-        scaled_depth_image: np.ndarray = (depth_image * depth_scale_factor).astype(np.uint16)
-        cv2.imwrite(filename, scaled_depth_image)
+        cv2.imwrite(filename, ImageUtil.to_short_depth(depth_image, depth_scale_factor=depth_scale_factor))
+
+    @staticmethod
+    def to_short_depth(depth_image: np.ndarray, *, depth_scale_factor: float = 1000) -> np.ndarray:
+        """
+        Convert a floating-point depth image to an unsigned short one.
+
+        :param depth_image:         The floating-point depth image.
+        :param depth_scale_factor:  The factor by which to multiply the depths during the conversion.
+        :return:                    The unsigned short depth image.
+        """
+        return (depth_image * depth_scale_factor).astype(np.uint16)
