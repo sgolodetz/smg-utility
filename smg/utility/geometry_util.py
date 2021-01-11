aff9d30b1ffa3bc6c2892c9c964dcc353eed3819
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-11 09:31:50 +0000

    Move pyoctomap's compute_world_points_image to GeometryUtil.

--- smg/utility/geometry_util.py
+++ smg/utility/geometry_util.py
@@ -38,6 +38,31 @@ class GeometryUtil:
         )
 
     @staticmethod
+    def compute_world_points_image_fast(depth_image: np.ndarray, pose: np.ndarray,
+                                        intrinsics: Tuple[float, float, float, float],
+                                        ws_points: np.ndarray) -> None:
+        """
+        Compute a world-space points image from a depth image, camera pose, and set of camera intrinsics.
+
+        .. note::
+            The origin, i.e. (0, 0, 0), is stored for pixels whose depth is zero.
+
+        :param depth_image:     The depth image (pixels with zero depth are treated as invalid).
+        :param pose:            The camera pose (denoting a transformation from camera space to world space).
+        :param intrinsics:      The camera intrinsics, as an (fx, fy, cx, cy) tuple.
+        :param ws_points:       The output world-space points image.
+        """
+        # TODO: This should ultimately replace the compute_world_points_image function above.
+        height, width = depth_image.shape
+        fx, fy, cx, cy = intrinsics
+        xl = np.array(range(width))
+        yl = np.array(range(height))
+        al = np.tile((xl - cx) / fx, height).reshape(height, width) * depth_image
+        bl = np.transpose(np.tile((yl - cy) / fy, width).reshape(width, height)) * depth_image
+        for i in range(3):
+            ws_points[:, :, i] = pose[i, 0] * al + pose[i, 1] * bl + pose[i, 2] * depth_image
+
+    @staticmethod
     def make_depths_orthogonal(depth_image: np.ndarray, intrinsics: Tuple[float, float, float, float]) -> None:
         """
         Convert the depth values in a depth image from Euclidean distances from the camera centre
