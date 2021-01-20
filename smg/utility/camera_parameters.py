1eb7c0ff1a60746e28674232eb1006be423bc3c2
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-01-20 09:20:45 +0000

    Finish working on CameraParameters.

--- smg/utility/camera_parameters.py
+++ smg/utility/camera_parameters.py
@@ -6,12 +6,12 @@ from typing import Any, Dict, Optional, Tuple
 
 
 class CameraParameters:
-    """The parameters for one or more cameras."""
+    """A set of camera parameters for one or more cameras."""
 
     # CONSTRUCTOR
 
     def __init__(self):
-        """TODO"""
+        """Construct a set of camera parameters."""
         self.__data: Dict[str, Dict[str, Any]] = {}
 
     # PUBLIC STATIC METHODS
@@ -19,21 +19,34 @@ class CameraParameters:
     @staticmethod
     def load(filename: str) -> CameraParameters:
         """
-        TODO
+        Load a set of camera parameters from a JSON file.
 
-        :param filename:    TODO
-        :return:            TODO
+        :param filename:    The name of the file.
+        :return:            The loaded camera parameters.
         """
         return CameraParameters().__load(filename)
 
     # PUBLIC METHODS
 
+    def delete(self, camera_name: str) -> bool:
+        """
+        Delete the parameters for the camera with the specified name (if it exists).
+
+        :param camera_name: The name of a camera.
+        :return:            True, if the camera existed, or False otherwise.
+        """
+        if self.__data.get(camera_name) is not None:
+            del self.__data[camera_name]
+            return True
+        else:
+            return False
+
     def get_image_size(self, camera_name: str) -> Tuple[int, int]:
         """
-        TODO
+        Get the size of the images produced by the camera with the specified name.
 
-        :param camera_name: TODO
-        :return:            TODO
+        :param camera_name: The name of a camera.
+        :return:            The size of the images produced by the camera, as a (width, height) tuple.
         """
         camera_data: Optional[Dict[str, Any]] = self.__data.get(camera_name)
         if camera_data is not None:
@@ -43,10 +56,10 @@ class CameraParameters:
 
     def get_intrinsics(self, camera_name: str) -> Tuple[float, float, float, float]:
         """
-        TODO
+        Get the intrinsics of the camera with the specified name.
 
-        :param camera_name: TODO
-        :return:            TODO
+        :param camera_name: The name of a camera.
+        :return:            The intrinsics of the camera.
         """
         camera_data: Optional[Dict[str, Any]] = self.__data.get(camera_name)
         if camera_data is not None:
@@ -56,24 +69,24 @@ class CameraParameters:
 
     def save(self, filename: str) -> None:
         """
-        TODO
+        Save the camera parameters to a JSON file.
 
-        :param filename:    TODO
+        :param filename:    The name of the file.
         """
         with open(filename, "w") as f:
             json.dump(self.__data, f, indent=4)
 
     def set(self, camera_name: str, width: int, height: int, fx: float, fy: float, cx: float, cy: float) -> None:
         """
-        TODO
+        Set the parameters of the camera with the specified name.
 
-        :param camera_name: TODO
-        :param width:       TODO
-        :param height:      TODO
-        :param fx:          TODO
-        :param fy:          TODO
-        :param cx:          TODO
-        :param cy:          TODO
+        :param camera_name: The name of the camera.
+        :param width:       The width of the camera's images.
+        :param height:      The height of the camera's images.
+        :param fx:          The horizontal focal length of the camera.
+        :param fy:          The vertical focal length of the camera.
+        :param cx:          The x component of the camera's principal point.
+        :param cy:          The y component of the camera's principal point.
         """
         self.__data[camera_name] = {"width": width, "height": height, "fx": fx, "fy": fy, "cx": cx, "cy": cy}
 
@@ -81,10 +94,10 @@ class CameraParameters:
 
     def __load(self, filename: str) -> CameraParameters:
         """
-        TODO
+        Load a set of camera parameters from a JSON file.
 
-        :param filename:    TODO
-        :return:            TODO
+        :param filename:    The name of the file.
+        :return:            The current object.
         """
         with open(filename, "r") as f:
             self.__data = json.load(f)
