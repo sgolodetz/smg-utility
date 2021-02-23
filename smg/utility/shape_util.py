9060b988863f4c1009838a58b8f7531c8fa82600
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-23 16:51:04 +0000

    Add more comments.

--- smg/utility/shape_util.py
+++ smg/utility/shape_util.py
@@ -6,29 +6,48 @@ from .shapes import SC_OUTSIDE, Shape
 
 
 class ShapeUtil:
-    """TODO"""
+    """Utility functions related to geometric shapes."""
 
     # PUBLIC STATIC METHODS
 
     @staticmethod
     def rasterise_shapes(shapes: List[Shape], voxel_size: float) -> List[np.ndarray]:
+        """
+        Make a list of voxels that potentially intersect/touch at least one of the specified shapes.
+
+        .. note::
+            The "potentially" refers to the fact that we test the bounding spheres of the voxels (rather than
+            the voxels themselves) against the shapes, for speed reasons.
+        .. note::
+            As currently implemented, the output list may contain duplicates.
+
+        :param shapes:      A list of shapes.
+        :param voxel_size:  The voxel size.
+        :return:            A list of voxels that potentially intersect/touch at least one of the specified shapes.
+        """
         output: List[np.ndarray] = []
 
+        # For each shape:
         for shape in shapes:
+            # Round the shape bounds to the nearest voxel coordinates (away from zero).
             mins: np.ndarray = np.floor(shape.mins() / voxel_size) * voxel_size
             maxs: np.ndarray = np.ceil(shape.maxs() / voxel_size) * voxel_size
 
+            # Work out the x, y and z values for the centres of the voxels that will need to be tested.
             vals: Dict[int, np.ndarray] = {}
             for i in range(3):
                 vals[i] = np.linspace(
                     mins[i] + voxel_size / 2, maxs[i] - voxel_size / 2, int(np.round((maxs[i] - mins[i]) / voxel_size))
                 )
 
+            # For each voxel that needs to be tested:
             for z in vals[2]:
                 for y in vals[1]:
                     for x in vals[0]:
-                        p: np.ndarray = np.array([x, y, z])
-                        if shape.classify_sphere(p, np.sqrt(3) * voxel_size / 2) != SC_OUTSIDE:
-                            output.append(p)
+                        # Test its bounding sphere against the shape. If it intersects/touches the shape,
+                        # add the voxel to the output list.
+                        voxel_centre: np.ndarray = np.array([x, y, z])
+                        if shape.classify_sphere(voxel_centre, np.sqrt(3) * voxel_size / 2) != SC_OUTSIDE:
+                            output.append(voxel_centre)
 
         return output
