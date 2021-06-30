d82b693ef3ea799adc3c4881fd2c39b6e13cca7a
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-06-30 10:56:56 +0100

    Try filtering out isolated depth regions.

---
+++ smg/utility/depth_image_processor.py
@@ -0,0 +1,111 @@
+import cv2
+import math
+import numpy as np
+
+from numba import cuda
+from timeit import default_timer as timer
+from typing import Dict, Tuple
+
+from smg.utility import ImageUtil, NumbaUtil
+
+
+class DepthImageProcessor:
+    """Utility functions for post-processing depth images."""
+
+    # PUBLIC STATIC METHODS
+
+    @staticmethod
+    def remove_isolated_regions(depth_image: np.ndarray, segmentation: np.ndarray, stats: np.ndarray,
+                                *, min_region_size: int) -> Tuple[np.ndarray, np.ndarray]:
+        replace = {}  # type: Dict[int, int]
+        for i in range(stats.shape[0]):
+            area = stats[i, cv2.CC_STAT_AREA]
+            if area < min_region_size:
+                replace[i] = 0
+
+        indexer = np.array([replace.get(i, i) for i in range(np.min(segmentation), np.max(segmentation) + 1)])
+
+        segmentation = indexer[segmentation]
+        depth_image = np.where(segmentation != 0, depth_image, 0.0)
+
+        return depth_image, segmentation
+
+    @staticmethod
+    def segment_depth_image(depth_image: np.ndarray, *, threshold: float) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        # Make the depth edge map.
+        depth_edges = np.full_like(depth_image, 255, dtype=np.uint8)  # type: np.ndarray
+        DepthImageProcessor.__make_depth_edge_map(depth_image, depth_edges, threshold)
+
+        # Find the connected components of the depth edge map.
+        _, segmentation, stats, _ = cv2.connectedComponentsWithStats(depth_edges)
+
+        return segmentation, stats, depth_edges
+
+    # PRIVATE STATIC METHODS
+
+    @staticmethod
+    def __make_depth_edge_map(depth_image, depth_edges, threshold: float) -> None:
+        NumbaUtil.launch_kernel_2d(
+            DepthImageProcessor.__ck_make_depth_edge_map, depth_image, depth_edges, threshold,
+            grid_size=depth_image.shape
+        )
+
+    # PRIVATE STATIC CUDA KERNELS
+
+    @staticmethod
+    @cuda.jit
+    def __ck_make_depth_edge_map(depth_image, depth_edges, threshold: float):
+        # noinspection PyArgumentList
+        cy, cx = cuda.grid(2)
+        if cy < depth_image.shape[0] and cx < depth_image.shape[1]:
+            cdepth: float = depth_image[cy, cx]
+
+            k_squared: int = 25
+            k: int = int(math.sqrt(float(k_squared)))
+            centre: int = k_squared // 2
+            half_k: int = k // 2
+
+            # For each potential neighbour of the current pixel:
+            for i in range(k_squared):
+                dy, dx = divmod(i, k)
+                x = cx - half_k + dx
+                y = cy - half_k + dy
+
+                # Skip the current pixel and any neighbours that are outside the image bounds.
+                if not(i != centre and 0 <= y < depth_image.shape[0] and 0 <= x < depth_image.shape[1]):
+                    continue
+
+                if math.fabs(depth_image[y, x] - cdepth) > threshold:
+                    depth_edges[cy, cx] = 0
+                    break
+
+
+def main() -> None:
+    depth_image: np.ndarray = ImageUtil.load_depth_image("D:/jiaxing/frame-000000.depth.png")
+
+    start = timer()
+
+    segmentation, stats, depth_edges = DepthImageProcessor.segment_depth_image(
+        depth_image, threshold=0.02
+    )
+
+    end = timer()
+    print(end - start)
+
+    start = timer()
+
+    depth_image, segmentation = DepthImageProcessor.remove_isolated_regions(
+        depth_image, segmentation, stats, min_region_size=500
+    )
+
+    end = timer()
+    print(end - start)
+
+    cv2.imshow("Depth Image", depth_image / 2)
+    cv2.imshow("Depth Edges", depth_edges)
+    cv2.imshow("Segmentation", ImageUtil.colourise_segmentation(segmentation))
+    cv2.waitKey()
+
+
+if __name__ == "__main__":
+    main()
--- smg/utility/image_util.py
+++ smg/utility/image_util.py
@@ -1,7 +1,13 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-from typing import Any
+from numba import cuda
+from PIL import Image
+from typing import Any, Optional
+
+from .numba_util import NumbaUtil
 
 
 # MAIN CLASS
@@ -9,7 +15,44 @@ from typing import Any
 class ImageUtil:
     """Utility functions related to images."""
 
+    # PRIVATE STATIC CONSTANTS
+
+    __CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
     # PUBLIC STATIC METHODS
+
+    @staticmethod
+    def colourise_segmentation(segmentation: np.ndarray, *, may_load_default_palette: bool = True,
+                               palette: Optional[np.ndarray] = None) -> np.ndarray:
+        """
+        Make a colourised version of the specified segmentation.
+
+        .. note::
+            If a palette is passed in, it will be used. If not, but this function is allowed to load the
+            default palette, that will be used. Otherwise, a colour map from pyplot wil be used.
+
+        :param segmentation:                The segmentation to colourise.
+        :param may_load_default_palette:    Whether or not this function is allowed to load the default palette
+                                            if no palette is passed in.
+        :param palette:                     An optional palette.
+        :return:                            The colourised segmentation.
+        """
+        # If no palette was passed in, but we're allowed to load the default palette, load it.
+        if palette is None and may_load_default_palette:
+            palette = ImageUtil.load_default_palette()
+
+        # If we now have a palette, use it to colourise the segmentation. If not, use a colour map from pyplot
+        # to colourise the segmentation instead.
+        if palette is not None:
+            height, width = segmentation.shape[:2]
+            segmentation_bgr = np.zeros((height, width, 3), dtype=np.uint8)  # type: np.ndarray
+            NumbaUtil.launch_kernel_2d(
+                ImageUtil.__ck_apply_palette, palette, segmentation, segmentation_bgr, grid_size=(height, width)
+            )
+            return segmentation_bgr
+        else:
+            cm = plt.get_cmap("tab20")
+            return (ImageUtil.flip_channels(cm(np.mod(segmentation, 20))) * 255).astype(np.uint8)
 
     @staticmethod
     def fill_border(image: np.ndarray, border_size: int, value: Any) -> np.ndarray:
@@ -51,6 +94,16 @@ class ImageUtil:
         return short_depth_image / depth_scale_factor
 
     @staticmethod
+    def load_default_palette() -> np.ndarray:
+        """
+        Load the default palette.
+
+        :return:    The default palette.
+        """
+        palette_image_pil = Image.open(os.path.join(ImageUtil.__CURRENT_DIR, "palette.png"))
+        return np.array(palette_image_pil.getpalette()).reshape((256, 3))
+
+    @staticmethod
     def load_depth_image(filename: str, *, depth_scale_factor: float = 1000.0) -> np.ndarray:
         """
         Load a depth image from disk.
@@ -84,3 +137,27 @@ class ImageUtil:
         :return:                    The unsigned short depth image.
         """
         return (depth_image * depth_scale_factor).astype(np.uint16)
+
+    # PRIVATE STATIC CUDA KERNELS
+
+    @staticmethod
+    @cuda.jit
+    def __ck_apply_palette(palette, segmentation, segmentation_bgr) -> None:
+        """
+        Apply a palette to the specified segmentation to produce a colourised version of it.
+
+        :param palette:             The palette.
+        :param segmentation:        The segmentation.
+        :param segmentation_bgr:    An output image into which to write the colourised version of the segmentation.
+        """
+        # noinspection PyArgumentList
+        y, x = cuda.grid(2)
+
+        if y < segmentation.shape[0] and x < segmentation.shape[1]:
+            # Determine the colour from the palette to use for this pixel.
+            idx = segmentation[y, x] % palette.shape[0]  # type: int
+
+            # Write the specified colour into the relevant pixel of the output image. Note that the colours in
+            # the palette are RGB, whereas our output image is BGR, so we flip the channels when doing this.
+            for i in range(3):
+                segmentation_bgr[y, x, i] = palette[idx, 2 - i]
