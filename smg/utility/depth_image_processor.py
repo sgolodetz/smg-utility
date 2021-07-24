176d582764ed076fc2890b638b3a2de4054eb0e3
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-07-24 16:07:38 +0100

    Introduce the try_depth_image_processor script.

--- smg/utility/depth_image_processor.py
+++ smg/utility/depth_image_processor.py
@@ -3,10 +3,8 @@ import math
 import numpy as np
 
 from numba import cuda
-from timeit import default_timer as timer
 from typing import Dict, Tuple
 
-from .image_util import ImageUtil
 from .numba_util import NumbaUtil
 
 
@@ -79,34 +77,3 @@ class DepthImageProcessor:
                 if math.fabs(depth_image[y, x] - cdepth) > threshold:
                     depth_edges[cy, cx] = 0
                     break
-
-
-def main() -> None:
-    depth_image: np.ndarray = ImageUtil.load_depth_image("D:/jiaxing/frame-000000.depth.png")
-
-    start = timer()
-
-    segmentation, stats, depth_edges = DepthImageProcessor.segment_depth_image(
-        depth_image, threshold=0.02
-    )
-
-    end = timer()
-    print(end - start)
-
-    start = timer()
-
-    depth_image, segmentation = DepthImageProcessor.remove_isolated_regions(
-        depth_image, segmentation, stats, min_region_size=500
-    )
-
-    end = timer()
-    print(end - start)
-
-    cv2.imshow("Depth Image", depth_image / 2)
-    cv2.imshow("Depth Edges", depth_edges)
-    cv2.imshow("Segmentation", ImageUtil.colourise_segmentation(segmentation))
-    cv2.waitKey()
-
-
-if __name__ == "__main__":
-    main()
